95c2e949d638f862f27696c2c009d56c313a27bb
Pablo <Pablo@NTB003ITC>
2021-08-23 21:48:50 -0300

    Agregar archivos de proyecto.

---
+++ Primera_Aplicacion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Primera_Aplicacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = Console.ReadLine();
+            Console.WriteLine("Su nombre es: ",nombre1);
+           
+            int edadIngresada = Console.ReadLine();
+            int edad = int.Parse(edadIngresada);
+
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            Console.WriteLine("Ingrese un nombre");
+        }
+    }
+}
